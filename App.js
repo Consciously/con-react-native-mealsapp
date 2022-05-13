b5ff3de0220e500b81b218e643c5ad59294efffb
Stefan Ihle <stefanihle@gmx.net>
2022-05-13 15:00:37 +0200

    style categories grid screen and implement navigation

--- App.js
+++ App.js
@@ -1,8 +1,22 @@
+import { StatusBar } from 'expo-status-bar';
 import { StyleSheet } from 'react-native';
 import CategoriesScreen from './screens/CategoriesScreen';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+const Stack = createNativeStackNavigator();
 
 const App = () => {
-	return <CategoriesScreen />;
+	return (
+		<>
+			<StatusBar style='dark' />
+			<NavigationContainer>
+				<Stack.Navigator>
+					<Stack.Screen name='MealCategories' component={CategoriesScreen} />
+				</Stack.Navigator>
+			</NavigationContainer>
+		</>
+	);
 };
 
 export default App;
--- screens/CategoriesScreen.js
+++ screens/CategoriesScreen.js
@@ -14,6 +14,7 @@ const CategoriesScreen = () => {
 			data={CATEGORIES}
 			keyExtractor={item => item.id}
 			renderItem={renderCategoryItem}
+			numColumns={2}
 		/>
 	);
 };
