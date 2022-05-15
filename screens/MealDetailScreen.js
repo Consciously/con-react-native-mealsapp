768d66c2b13e4f6f42a628e52c281eee6fc2d5ef
Stefan Ihle <stefanihle@gmx.net>
2022-05-15 23:27:48 +0200

    refactor meal deatils and give appropriate styles

--- screens/MealDetailScreen.js
+++ screens/MealDetailScreen.js
@@ -1,6 +1,8 @@
-import { View, Image, Text } from 'react-native';
+import { View, Image, Text, StyleSheet, ScrollView } from 'react-native';
 import { MEALS } from '../data/dummy-data';
 import MealDetails from '../components/MealDetails';
+import Subtitle from '../components/MealDetail/Subtitle';
+import List from '../components/MealDetail/List';
 
 const MealDetailScreen = ({ route }) => {
 	const mealId = route.params.mealId;
@@ -14,24 +16,51 @@ const MealDetailScreen = ({ route }) => {
 	};
 
 	return (
-		<View>
+		<ScrollView style={styles.rootContainer}>
 			<Image
+				style={styles.image}
 				source={{
 					uri: selectedMeal.imageUrl
 				}}
 			/>
-			<Text>{selectedMeal.title}</Text>
-			<MealDetails {...mealDetailsProps} />
-			<Text>Ingredients</Text>
-			{selectedMeal.ingredients.map(ingredient => (
-				<Text key={ingredient}>{ingredient}</Text>
-			))}
-			<Text>Steps</Text>
-			{selectedMeal.steps.map(step => (
-				<Text key={step}>{step}</Text>
-			))}
-		</View>
+			<Text style={styles.title}>{selectedMeal.title}</Text>
+			<MealDetails {...mealDetailsProps} textStyle={styles.detailText} />
+			<View style={styles.listOuterContainer}>
+				<View style={styles.listContainer}>
+					<Subtitle>Ingredients</Subtitle>
+					<List data={selectedMeal.ingredients} />
+					<Subtitle>Steps</Subtitle>
+					<List data={selectedMeal.steps} />
+				</View>
+			</View>
+		</ScrollView>
 	);
 };
 
 export default MealDetailScreen;
+
+const styles = StyleSheet.create({
+	rootContainer: {
+		marginBottom: 32
+	},
+	image: {
+		width: '100%',
+		height: 350
+	},
+	title: {
+		fontWeight: 'bold',
+		fontSize: 24,
+		margin: 8,
+		textAlign: 'center',
+		color: 'white'
+	},
+	detailText: {
+		color: 'white'
+	},
+	listOuterContainer: {
+		alignItems: 'center'
+	},
+	listContainer: {
+		width: '80%'
+	}
+});
