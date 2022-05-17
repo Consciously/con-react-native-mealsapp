f6554a1bb268a07b965b4b78360273a1b7af60bd
Stefan Ihle <stefanihle@gmx.net>
2022-05-17 15:59:55 +0200

    finetune styling for drawer

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet } from 'react-native';
+import { Ionicons } from '@expo/vector-icons';
 import CategoriesScreen from './screens/CategoriesScreen';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
@@ -17,15 +18,32 @@ const DrawerNavigator = () => {
 			screenOptions={{
 				headerStyle: { backgroundColor: '#351401' },
 				headerTintColor: 'white',
-				sceneContainerStyle: { backgroundColor: '#3f2f25' }
+				sceneContainerStyle: { backgroundColor: '#3f2f25' },
+				drawerContentStyle: { backgroundColor: '#351401' },
+				drawerInactiveTintColor: 'white',
+				drawerActiveTintColor: '#351401',
+				drawerActiveBackgroundColor: '#e4baa1'
 			}}
 		>
 			<Drawer.Screen
 				name='Categories'
 				component={CategoriesScreen}
-				options={{ title: 'All Categories' }}
+				options={{
+					title: 'All Categories',
+					drawerIcon: ({ color, size }) => (
+						<Ionicons name='list' color={color} size={size} />
+					)
+				}}
 			/>
-			<Drawer.Screen name='Favorites' component={FavoritesScreen} />
+			<Drawer.Screen
+				name='Favorites'
+				component={FavoritesScreen}
+				options={{
+					drawerIcon: ({ color, size }) => (
+						<Ionicons name='star' color={color} size={size} />
+					)
+				}}
+			/>
 		</Drawer.Navigator>
 	);
 };
